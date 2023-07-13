ab8d5339fcf1b5c276a7762c9c44917396443cec
Caylin James <cjames@leapagency.com>
2023-07-13 10:38:06 -0400

    finish up quick look at annotating props with typescript

--- src/props/Child.tsx
+++ src/props/Child.tsx
@@ -1,11 +1,23 @@
 interface ChildProps {
     color: string;
+    onClick: () => void;
+    children?: React.ReactNode;
 }
 
-export const Child = ({ color }: ChildProps) => {
-    return <div>{color}</div>;
+export const Child = ({ color, onClick }: ChildProps) => {
+    return (
+        <div>
+            {color}
+            <button onClick={onClick}>Click Me</button>
+        </div>
+    );
 };
 
-export const ChildAsFc: React.FC<ChildProps> = ({ color }) => {
-    return <div>{color}</div>;
+export const ChildAsFc: React.FC<ChildProps> = ({ color, onClick }) => {
+    return (
+        <div>
+            {color}
+            <button onClick={onClick}>Click Me</button>
+        </div>
+    );
 };
--- src/props/Parent.tsx
+++ src/props/Parent.tsx
@@ -1,7 +1,16 @@
 import { Child } from "./Child";
 
 const Parent = () => {
-    return <Child color="green" />;
+    return (
+        <Child
+            color="green"
+            onClick={() => {
+                console.log("clicked");
+            }}
+        >
+            <div>Oh hai</div>
+        </Child>
+    );
 };
 
 export default Parent;
