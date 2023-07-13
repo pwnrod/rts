e5a8e5a29d1a369f8e2301c1c2d33d0b0d137ea3
Caylin James <cjames@leapagency.com>
2023-07-13 10:58:13 -0400

    add the UserSearch component for another example of using state with TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import GuestList from "./state/GuestList";
+import UserSearch from "./state/UserSearch";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
     return (
         <div>
-            <GuestList />
+            <UserSearch />
         </div>
     );
 };
