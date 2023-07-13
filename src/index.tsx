dc0cdbebfcaa6001644c5b03d69503a925e43490
Caylin James <caylinjames.dev@gmail.com>
2023-07-13 18:59:10 -0400

    wrap up on refs

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import EventComponent from "./events/EventComponent";
+import UserSearch from "./refs/UserSearch";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el!);
@@ -7,7 +7,7 @@ const root = ReactDOM.createRoot(el!);
 const App = () => {
     return (
         <div>
-            <EventComponent />
+            <UserSearch />
         </div>
     );
 };
